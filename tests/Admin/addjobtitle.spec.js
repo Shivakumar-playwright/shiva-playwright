00ada701ecf25adf9d2c927f90e23f53b74a2060
shivakumar-playwright <tshivak399@gmail.com>
2025-06-11 19:49:30 +0530

    adding js

--- tests/Admin/addjobtitle.spec.js
+++ tests/Admin/addjobtitle.spec.js
@@ -13,10 +13,11 @@ test("verify login functionality and adding job title ", async ({ page }) => {
     await page.locator('(//li[@class="oxd-topbar-body-nav-tab --parent"])[1]').click()
     await page.locator('(//a[@class="oxd-topbar-body-nav-tab-link"])[1]').click()
     await page.locator('button[class="oxd-button oxd-button--medium oxd-button--secondary"]').click()
-    await page.locator('(//input[@class="oxd-input oxd-input--active"])[2]').fill(data.jobtitle)
-    await page.locator('textarea[placeholder="Type description here"]').fill(data['job description'])
+    let r = (Math.random() + 1).toString(36).substring(7);  //generate random 5 charecters
+    await page.locator('(//input[@class="oxd-input oxd-input--active"])[2]').fill(data.jobtitle+r)
+    await page.locator('textarea[placeholder="Type description here"]').fill(data['job description']+r)
     await page.locator('textarea[placeholder="Add note"]').fill(data['add note'])
     await page.locator('button[type="submit"]').click()
     await expect(page.locator('h6[class="oxd-text oxd-text--h6 orangehrm-main-title"]')).toBeVisible()
-
+    await page.close()
 })
