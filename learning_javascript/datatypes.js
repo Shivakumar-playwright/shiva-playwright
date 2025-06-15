62eaf8238f44e543242d40bd293b848a5b95e23b
shivakumar-playwright <tshivak399@gmail.com>
2025-06-15 08:50:36 +0530

    javascript

---
+++ learning_javascript/datatypes.js
@@ -0,0 +1,14 @@
+let number=10
+console.log(typeof(number))
+
+let n="20";
+console.log(typeof(n))
+
+let str;
+console.log(typeof(str))
+
+let a=null
+console.log(typeof(a))
+
+let b=false;
+console.log(typeof(b))
---
+++ learning_javascript/variables.js
@@ -0,0 +1,28 @@
+// varialbes are containers for storing data;
+
+// variables are declare in 3 ways
+// 1.var
+// 2.let 
+// 3.const 
+
+var x=10;
+   x=20;
+   {
+   var x=30;
+   }
+console.log(x)
+
+
+let y=100;
+ y=21;
+ {
+   let y=231;
+ }
+console.log(y)
+
+//hoisting:declare a variable before declaration
+console.log(z)
+var z=90;
+
+// console.log(num)
+// let num=1100;         //reference error
