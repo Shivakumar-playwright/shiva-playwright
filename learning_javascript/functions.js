399171f4ed498992adf68f8e96e2a9c1157496d1
shivakumar-playwright <tshivak399@gmail.com>
2025-06-18 17:52:31 +0530

    functions in javascript

---
+++ learning_javascript/functions.js
@@ -0,0 +1,65 @@
+//FUNCTIONS:functions are set of statements wrapped inside the block that perform some actions or task
+
+// 1.Normal function:functions without parameter
+
+function greet() {
+    console.log("Hello, how are you?");
+}
+greet()
+
+// 2.parameter function:functions with parameter 
+
+function add(x, y) {
+    var result = x + y;
+    console.log(result)
+}
+add(5, 6)
+add(2, 7)
+
+// 3.anonymous function:A function without name 
+const greett = function () {
+    console.log("hello")
+}
+greett()
+
+// 4.return function 
+function adding(x, y) {
+    return x + y
+}
+console.log(adding(5, 6))
+
+//5.arrow function:short way to write a function using the => arrow symbol
+
+const sub=(x,y)=>{
+    console.log(x-y)
+}
+sub(5, 6)
+
+const sqr=x=>x*x
+console.log(sqr(5))
+
+const mul=(x,y)=>{
+    var res=x**y
+    console.log(res)
+}
+mul(2,3)
+
+//6.Callback function:A function passed as an argument to another function
+
+function gret(name,callback){
+    console.log("hello"+name)
+    callback()
+}
+function saybye(){
+    console.log("bye")
+}
+gret("shiva",saybye)
+
+//7.High-Order-Function:take another function as a parameter and return a function
+function sayhello(){
+    console.log("hello shiva kumar")
+}
+function greetuser(callback){
+    callback()
+}
+greetuser(sayhello)
--- tests/PIM/datadriveaddemploye.spec.js
+++ tests/PIM/datadriveaddemploye.spec.js
@@ -1,15 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 const employeedata = {
-    employee1: { firstname: "shiva", lastname: "kumar" },
-    employee2: { firstname: "babu", lastname: "r" },
-    employee3: { firstname: "tulasi", lastname: "ram" },
-    employee4: { firstname: "raju", lastname: "t" },
-    employee5: { firstname: "maruthi", lastname: "n" },
+    employee1: { firstname: "shiva", middlename: "kumar", lastname: "T" },
+    employee2: { firstname: "babu", middlename: "prasad", lastname: "R" },
+    employee3: { firstname: "tulasi", middlename: "ram", lastname: "A" },
+    employee4: { firstname: "raju", middlename: "sir", lastname: "t" },
+    employee5: { firstname: "maruthi", middlename: "varaprasad", lastname: "N" },
 };
 for (let key in employeedata) {
 
-    test(`Add employee: ${employeedata[key].firstname} ${employeedata[key].lastname}`, async ({ page }) => {
+    test(`Add employee: ${key}`, async ({ page }) => {
         await page.goto("/web/index.php/auth/login")
         await page.locator('input[placeholder="Username"]').fill(process.env.ORG_USERNAME)
         await page.locator('input[name="password"]').fill(process.env.ORG_PASSWORD)
@@ -18,6 +18,7 @@ for (let key in employeedata) {
         await page.locator('(//span[@class="oxd-text oxd-text--span oxd-main-menu-item--name"])[2]').click()
         await page.locator('(//a[@class="oxd-topbar-body-nav-tab-item"])[2]').click()
         await page.locator('input[placeholder="First Name"]').fill(employeedata[key].firstname)
+        await page.locator('input[placeholder="Middle Name"]').fill(employeedata[key].middlename)
         await page.locator('input[placeholder="Last Name"]').fill(employeedata[key].lastname)
         await page.locator('button[type="submit"]').click()
         await expect(page.locator('(//h6[@class="oxd-text oxd-text--h6 orangehrm-main-title"])[1]')).toBeVisible()
