ec4f5e11da41253ba9986d5012c23c72065d05c9
shivakumar-playwright <tshivak399@gmail.com>
2025-06-19 20:31:29 +0530

    arrays in javascript

--- tests/conditionalstatementex.spec.js
+++ tests/conditionalstatementex.spec.js
@@ -3,16 +3,16 @@ import { test, expect } from '@playwright/test';
 test('checking disappearing elements', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/disappearing_elements');
 
-    const gallery = page.locator('a[href="/gallery/"]');
-    const portfolio = page.locator('a[href="/portfolio/"]');
+    const gallery = await page.locator('a[href="/gallery/"]');
+    const portfolio = await page.locator('a[href="/portfolio/"]');
 
-    if (await gallery.isVisible()) {
-        await gallery.click();
+    if ( gallery.isVisible()) {
+         gallery.click();
         console.log("gallery is clicked");
         await expect(page).toHaveURL('https://the-internet.herokuapp.com/gallery/');
         await page.close()
-    } else if (await portfolio.isVisible()) {
-        await portfolio.click();
+    } else if ( portfolio.isVisible()) {
+         portfolio.click();
         console.log("portfolio is clicked");
         await expect(page).toHaveURL('https://the-internet.herokuapp.com/portfolio/');
     }
